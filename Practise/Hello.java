4928e5bc7c1cd0a14726dca833bf9e11498a8394
Deepa-shree-tech <deepashree13102007@gmail.com>
2026-01-29 14:58:30 +0530

    Add files via upload

---
+++ Practise/Hello.java
@@ -0,0 +1,24 @@
+public class Hello{
+   public static void main(String[] args){
+      byte a=10;//-128 to 127
+      short b=20000;//-32768 to 32767
+      long c=6479327474L;//-9223372036854775808 to 9223372036854775807,L is mandatory
+      int d=100;//-2147483648 to 2147483647
+      float e=1.87f;//6-7 decimal places
+      double f=2.76435790876;//15-16 decimal places
+      char g='D';
+      boolean h=true;
+      System.out.println("Byte "+ a);
+      System.out.println("Short "+ b);
+      System.out.println("Long "+ c);
+      System.out.println("Integer "+ d);
+      System.out.println("Float "+ e);
+      System.out.println("Double "+ f);
+      System.out.println("Character "+ g);
+      System.out.println("Boolean "+ h);
+   }
+}
+
+
+      
+      
